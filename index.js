954347272e736a3c64ab529a2efeef06a86361ab
nikolka44 <zotikovich44@gmail.com>
2025-10-01 14:42:52 +0000

    Deploying to gh-pages from @ nikolka44/goit-js-hw-11@744e4c106fa78c80c63e5bdb4e5254a58a9a5029 🚀

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();console.log(123);
+import{a as y,S as h,i as a}from"./assets/vendor-BSTwZ_tR.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const s of i.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&n(s)}).observe(document,{childList:!0,subtree:!0});function r(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function n(e){if(e.ep)return;e.ep=!0;const i=r(e);fetch(e.href,i)}})();const b="52508688-b86b550d92da4de9dba40b9d1",v="https://pixabay.com/api/";function S(o){return y.get(v,{params:{key:b,q:o,image_type:"photo",orientation:"horizontal",safesearch:!0}}).then(t=>t.data).catch(t=>{throw console.log(t),new Error(t)})}const m=document.querySelector(".gallery"),d=document.querySelector(".loader");function L(o){const t=o.map(n=>{const{webformatURL:e,largeImageURL:i,tags:s,likes:f,views:p,comments:u,downloads:g}=n;return`<li class="list-item">
+      <a href="${i}" class="item-img-link">
+        <img class="gallery-img" src="${e}" alt="${s}" />
+      </a>
+      <div class="img-info-wrap">
+        <div class="img-info">
+          <p class="img-info-title">Likes</p>
+          <p class="img-info-content">${f}</p>
+        </div>
+        <div class="img-info">
+          <p class="img-info-title">Views</p>
+          <p class="img-info-content">${p}</p>
+        </div>
+        <div class="img-info">
+          <p class="img-info-title">Comments</p>
+          <p class="img-info-content">${u}</p>
+        </div>
+        <div class="img-info">
+          <p class="img-info-title">Downloads</p>
+          <p class="img-info-content">${g}</p>
+        </div>
+      </div>
+    </li>`}).join("");m.insertAdjacentHTML("beforeend",t),new h(".item-img-link",{captionsData:"alt",captionDelay:250,captionPosition:"bottom"}).refresh()}function w(){m.innerHTML=""}function q(){d.style.display="block"}function x(){d.style.display="none"}const c=document.querySelector(".form"),l=document.querySelector('button[type="submit"]'),O=document.querySelector('input[name="search-text"]');async function $(o){o.preventDefault();const t=O.value.trim();if(t===""){a.error({title:"",color:"red",message:"❌ Search field cannot be empty!",position:"topRight",messageSize:"18",icon:!1,progressBar:!1}),c.reset();return}else w(),q(),l.disabled=!0;try{const r=await S(t);if(!r.hits.length){a.error({title:"",color:"red",messageSize:"18",icon:!1,maxWidth:"432px",minHeight:"88px",progressBar:!1,message:"❌ Sorry, there are no images matching your search query. Please try again!",position:"topRight"});return}L(r.hits)}catch{a.error({title:"",color:"red",messageSize:"18",icon:!1,progressBar:!1,message:"❌ Sorry, network Error",position:"topRight"})}finally{setTimeout(()=>{c.reset()},1e3),l.disabled=!1,x()}}c.addEventListener("submit",$);
 //# sourceMappingURL=index.js.map
